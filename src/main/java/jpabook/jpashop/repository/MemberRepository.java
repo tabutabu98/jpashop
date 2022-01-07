733dde7cda7d9eace3c0940528886e34b54633e5
syc <syc@partdb.com>
2022-01-07 18:31:56 +0900

    feat 테스트 오류(회원 기능 테스트) @syc

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,8 +1,6 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -10,10 +8,8 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class MemberRepository {
-
-
+    @PersistenceContext
     private EntityManager em;
 
     public void save(Member member) {
@@ -30,7 +26,8 @@ public class MemberRepository {
     }
 
     public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+        return em.createQuery("select m from Member m where m.name = :name",
+                        Member.class)
                 .setParameter("name", name)
                 .getResultList();
     }
