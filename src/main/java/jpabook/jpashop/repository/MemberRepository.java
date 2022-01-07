78a99389ece69cb8602b7ff9d3c8a72db843cc9b
syc <syc@partdb.com>
2022-01-07 17:52:22 +0900

    feat 회원 서비스 개발 @syc

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,9 +10,10 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
+
     private EntityManager em;
 
     public void save(Member member) {
@@ -26,7 +29,7 @@ public class MemberRepository {
                 .getResultList();
     }
 
-    public List<Member> findByMember(String name) {
+    public List<Member> findByName(String name) {
         return em.createQuery("select m from Member m where m.name = :name", Member.class)
                 .setParameter("name", name)
                 .getResultList();
